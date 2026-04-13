e8d5d08d00d7075848413adea5ee5d0978b224d7
Yann.J <yann.judith@uqtr.ca>
2026-04-13 17:11:39 -0400

    InMemoryRepo creation

---
+++ TaskOrchestrator.Infrastructure/InMemoryTaskRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+using TaskOrchestrator.Application;
+using TaskOrchestrator.Domain;
+
+namespace TaskOrchestrator.Infrastructure;
+
+public class InMemoryTaskRepository: ITaskRepository
+{
+
+    private ConcurrentDictionary<Guid, OrchestratedTask> _concurrentDictionary = new ();
+
+    public async Task<OrchestratedTask?> GetAsync(Guid id, CancellationToken ct = default)
+    {
+        _concurrentDictionary.TryGetValue(id, out var task);
+        return task;
+    }
+    
+    public async Task<IReadOnlyList<OrchestratedTask>> GetAllAsync(CancellationToken ct = default)
+    {
+
+       return _concurrentDictionary.Values.ToList();
+    }
+
+    public async Task AddAsync(OrchestratedTask task, CancellationToken ct = default)
+    {
+        _concurrentDictionary.TryAdd(task.Id, task);
+    }
+
+    public async Task UpdateAsync(OrchestratedTask task, CancellationToken ct = default)
+    {
+        _concurrentDictionary.AddOrUpdate(task.Id, task, (key, existing) => task);
+
+    }
+}
