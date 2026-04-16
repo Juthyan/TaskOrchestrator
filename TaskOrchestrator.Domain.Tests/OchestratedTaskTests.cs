3243ff335ea4716a3cae3f6462719b25f6d64cde
Yann.J <yann.judith@uqtr.ca>
2026-04-16 10:09:55 -0400

    Test domain updated

--- TaskOrchestrator.Domain.Tests/OchestratedTaskTests.cs
+++ TaskOrchestrator.Domain.Tests/OchestratedTaskTests.cs
@@ -19,7 +19,7 @@ public class OrchestratedTaskTests
     {
         var task = OrchestratedTask.CreateNew(TaskType.Monitoring, maxAttempts: 3);
 
-        task.Running();
+        task.Start();
 
         Assert.Equal(TaskState.Running, task.Status);
     }
@@ -28,10 +28,10 @@ public class OrchestratedTaskTests
     public void Cannot_start_a_non_pending_task()
     {
         var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 3);
-        task.Running();
+        task.Start();
         task.Succeed();
 
-        Assert.Throws<DomainException>(() => task.Running());
+        Assert.Throws<DomainException>(() => task.Start());
     }
 
     [Fact]
@@ -39,17 +39,122 @@ public class OrchestratedTaskTests
     {
         var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 2);
 
-        task.Running();
-        task.Failed();
+        task.Start();
+        task.Fail();
 
         task.Retry();
 
         Assert.Equal(TaskState.Pending, task.Status);
         Assert.Equal(1, task.Attempts);
 
-        task.Running();
-        task.Failed();
+        task.Start();
+        task.Fail();
 
         Assert.Throws<DomainException>(() => task.Retry());
     }
+
+    [Fact]
+    public void Restart_must_put_the_attempts_to_zero()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 3);
+
+        task.Start();
+        task.Fail();
+
+        while (task.Attempts < 3)
+        {
+            task.Retry();
+            task.Start();
+            task.Fail();
+        }
+
+        Assert.Equal(TaskState.Failed, task.Status);
+        Assert.Equal(3, task.Attempts);
+
+        task.ReStart();
+
+        Assert.Equal(TaskState.Pending, task.Status);
+        Assert.Equal(0, task.Attempts);
+    }
+
+    [Fact]
+    public void Imposstible_to_restart_when_the_attempts_is_less_than_3()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 3);
+
+        task.Start();
+        task.Fail();
+
+        while (task.Attempts < 2)
+        {
+            task.Retry();
+            task.Start();
+            task.Fail();
+        }
+
+        Assert.Equal(TaskState.Failed, task.Status);
+        Assert.Equal(2, task.Attempts);
+        Assert.Throws<DomainException>(() =>  task.ReStart());
+    }
+
+     [Fact]
+    public void Imposstible_to_restart_a_task_not_failed()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 3);
+
+        task.Start();
+
+        Assert.Equal(TaskState.Running, task.Status);
+    
+        Assert.Throws<DomainException>(() =>  task.ReStart());
+    }
+
+    [Fact]
+    public void Cancel_only_available_for_pending_task()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 2);
+
+        Assert.Equal(TaskState.Pending, task.Status);
+
+        task.Cancel();
+
+        Assert.Equal(TaskState.Cancelled, task.Status);
+    }
+
+    [Fact]
+    public void Imposible_to_cancel_a_task_in_a_different_state_than_pending()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 2);
+
+        task.Start();
+
+        Assert.Equal(TaskState.Running, task.Status);
+
+        Assert.Throws<DomainException>(() =>  task.Cancel());
+    }
+
+    [Fact]
+    public void Archive_action_is_only_possible_for_succed_action()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 2);
+        task.Start();
+        task.Succeed();
+
+        Assert.Equal(TaskState.Succeeded, task.Status);
+
+        task.Archive();
+        Assert.Equal(TaskState.Archived, task.Status);
+    }
+    
+    [Fact]
+    public void Impossible_to_Archive_if_the_task_has_not_succeed()
+    {
+        var task = OrchestratedTask.CreateNew(TaskType.Simulation, maxAttempts: 2);
+        task.Start();
+
+        Assert.Equal(TaskState.Running, task.Status);
+
+        Assert.Throws<DomainException>(() => task.Archive());
+    }
+
 }
